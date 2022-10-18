60c091876b366501af34ff77e8e0f2c307988d10
LenzFF <113861256+LenzFF@users.noreply.github.com>
2022-10-18 11:56:15 +0300

    UpdateDepositCalculator.java
    Немного поправил)

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -1,7 +1,11 @@
 import java.util.Scanner;
 
 public class DepositCalculator {
-    double calculateComplexPercent(double depositAmount, double yearRate,int depositPeriod) {
+    //лучше добавить пустую строку
+    //yearRate у нас не меняется, можно вынести его сюда как контанту
+    
+    double calculateComplexPercent(double depositAmount, double yearRate,int depositPeriod) { //после запятой пробел
+        //лучше добавить пустую строку для читаемости
         double pay = depositAmount * Math.pow((1 + yearRate / 12), 12 * depositPeriod);
         return round(pay, 2);
     }
@@ -10,8 +14,8 @@ public class DepositCalculator {
         return round(depositAmount + depositAmount * yearRate * depositPeriod, 2);
     }
 
-    double round(double value,int places) {
-        double Scale = Math.pow(10, places); // возведение в степень 10^2
+    double round(double value,int places) {//пробел после запятой
+        double Scale = Math.pow(10, places); // возведение в степень 10^2 // ися переменной с маленькой буквы
         return Math.round(value * Scale) / Scale; //округляет до целого и делит на 100
     }
 
@@ -20,18 +24,22 @@ public class DepositCalculator {
         int depositType;
         Scanner scanner = new Scanner(System.in);
         double depositResult = 0;
-
+        //может здесь переменным дать имена покороче? код будет чуть компактенее.
+        //например amount, period, type..
+        
         System.out.println("Введите сумму вклада в рублях:");
         int depositAmount = scanner.nextInt();
         System.out.println("Введите срок вклада в годах:");
         depositPeriod = scanner.nextInt( );
         System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
         depositType = scanner.nextInt();
+        //лучше добавить пустую строку
         if (depositType == 1) {
             depositResult = calculateSimplePercent(depositAmount, 0.06, depositPeriod);
         } else if (depositType == 2) {
             depositResult = calculateComplexPercent(depositAmount, 0.06, depositPeriod);
         }
+        //можно пустую строку
         System.out.println("Результат вклада: " + depositAmount + " за " + depositPeriod + " лет превратятся в " + depositResult);
     }
 
